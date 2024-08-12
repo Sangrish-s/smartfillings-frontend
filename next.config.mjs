b230de5b082e2e33fdcf4a6adbf4b4270bbbb4a2
SangrishS <97099178+SangrishS@users.noreply.github.com>
2024-08-13 00:37:02 +0530

    Changing NExt config

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ const nextConfig = {
   env: {
     API_ENDPOINT: process.env.API_ENDPOINT,
   },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack(config, { isServer }) {
+    // Resolve modules from the src directory
+    config.resolve.modules.push(__dirname + "/src");
+    return config;
+  },
 };
 
 export default nextConfig;
