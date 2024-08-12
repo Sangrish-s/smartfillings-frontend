4176c6c855725e01ece9144ae3ddf31805e1c626
SangrishS <97099178+SangrishS@users.noreply.github.com>
2024-08-13 01:04:16 +0530

    Changing NExt config_2

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true
-  },
-  env: {
-    API_ENDPOINT: process.env.API_ENDPOINT,
-  },
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  webpack(config, { isServer }) {
-    // Resolve modules from the src directory
-    config.resolve.modules.push(__dirname + "/src");
-    return config;
-  },
-};
+const nextConfig = {};
 
-export default nextConfig;
+export default nextConfig
